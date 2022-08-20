8fd03ecf1d86c06067f982e8256a1d1e179abd1a
Erick Rodrigues <erickgamerh2o@gmail.com>
2022-08-19 22:04:56 -0300

    Introdução react: 19_08_2022

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './style.css'
 
 const App = () => {
-  return (
-    <div>
 
+  const estilosBotao = {marginTop:12, paddingTop:8, paddingBottom:8, backgroundColor:'blueviolet', color: 'white', border: 'none', width: '100%', borderRadius:8}
+  const textoDoRotulo = 'Nome : '
+  const obterTextoDoBotao = () => "Enviar"
+
+  return (
+    <div style={{margin: 'auto', width: 768, backgroundColor: '#EEE', padding: 12, borderRadius: 8}}>
+      <label className="rotulo" htmlFor="nome" > {textoDoRotulo} </label>
+      <input type="text" id="nome" style={{paddingTop: 8, paddingBottom: 8, borderStyle: 'hidden', width: '100%', borderRadius:8, outline: 'none'}}/>
+      <button style={estilosBotao}>{obterTextoDoBotao()}</button>
     </div>
   )
 }
 
 ReactDOM.render(
   <App />,
-  document.querySelector('#root')  
+  document.querySelector('#root')
 )
